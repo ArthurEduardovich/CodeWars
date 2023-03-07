35e0cb9bf25aa32ac2fc28d1328806691c74432f
ArthurEduardovich <artyr.eduardovich@gmail.com>
2023-03-07 11:02:34 +0500

    first-quest-07.03

---
+++ 07.03.2023.js
@@ -0,0 +1,31 @@
+/* The goal is to create a function of two inputs number and power, 
+that "raises" the number up to power (ie multiplies number by itself power times).
+
+Examples
+numberToPower(3, 2)  // -> 9 ( = 3 * 3 )
+numberToPower(2, 3)  // -> 8 ( = 2 * 2 * 2 )
+numberToPower(10, 6) // -> 1000000
+
+Note: Math.pow and some other Math functions like eval() and ** are disabled. */
+
+// function numberToPower(number, power){
+    // console.info(Math.log2(1024));
+    
+    
+  // }
+
+  const numberToPower = (number, power) => {
+    let result = 1;
+    if (number === 0 && power === 0) {
+        result = 0;
+    } else if (number > 0 && power === 0) {
+        result = 1;
+    } else if (number === 0 && power > 0){
+        result = 0;
+    }
+    for (let i = 1; i <= power; i++) {
+        result *= number
+    }
+    return result;
+}
+console.log(numberToPower(10, 6))
