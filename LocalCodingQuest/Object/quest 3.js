c79f84c01f60f61a9f26698a75a94177e241f0b6
ArthurEduardovich <artyr.eduardovich@gmail.com>
2023-03-15 18:04:14 +0500

    quest to operator && (AND)

--- LocalCodingQuest/Object/quest 3.js
+++ LocalCodingQuest/Object/quest 3.js
@@ -1,1 +1,25 @@
-function averageScore()
+function averageScore(exam1, exam2, exam3, exam4, exam5, userName){
+    let sum = 0;
+    let count = 0;
+    if (userName === exam1['name']) {
+        sum+= exam1['score'];
+        ++count;
+    }
+    if (userName === exam2['name']) {
+        sum+= exam2['score'];
+        ++count;
+    }
+    if (userName === exam3['name']) {
+        sum+= exam3['score'];
+        ++count;
+    }
+    if (userName === exam4['name']) {
+        sum+= exam4['score'];
+        ++count;
+    }
+    if (userName === exam5['name']) {
+        sum+= exam5['score'];
+        ++count;
+    }
+    return count ? sum / count: 0;
+}
