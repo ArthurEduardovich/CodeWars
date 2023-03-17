da9125f634155f953e0edeba5830adafc713c524
ArthurEduardovich <artyr.eduardovich@gmail.com>
2023-03-17 07:45:58 +0500

    rename directory

--- test.js
+++ test.js
@@ -1,6 +1,14 @@
+const yearDays = (year) => {
+    if (Math.abs(year) % 100 === 0 && Math.abs(year) % 4 === 0) {
+        return `${year} has is 365`;
+    }
+    if (Math.abs(year) % 100 !== 0 && Math.abs(year) % 4 === 0) {
+        return `${year} has is 366`;
+    }
+    return `${year} has is 365`;
+}
 
-
-
+console.log(yearDays(1500));
 
 
 
