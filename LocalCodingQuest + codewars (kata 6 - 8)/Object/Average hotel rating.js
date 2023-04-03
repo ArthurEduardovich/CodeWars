9472cc63c3d783e458a0b87792140c73228080f5
ArthurEduardovich <artyr.eduardovich@gmail.com>
2023-04-03 17:53:24 +0500

    new quest

--- LocalCodingQuest + codewars (kata 6 - 8)/Object/Average hotel rating.js
+++ LocalCodingQuest + codewars (kata 6 - 8)/Object/Average hotel rating.js
@@ -1,3 +1,4 @@
+// 1
 function getAverageRating(hotel){
     let stingHotel = JSON.stringify(hotel);
     let result = 0;
@@ -7,3 +8,78 @@ function getAverageRating(hotel){
     }
     return Number(result) / 4;
 }
+
+// 2
+function warehouseCapacity(capacity, productExist, productIncoming){
+    const object = {
+        warehouseAvailability: Math.round(capacity - productExist),
+        productIncoming: Math.round(productIncoming),
+        warehouseCapacity: 100 - (Math.round(((productExist + productIncoming) / capacity) * 100))
+    }
+    return object;
+}
+
+// 3
+
+function evenOddSum(n1,n2,n3,n4,n5){
+    const arr = [n1, n2, n3, n4, n5];
+    let odd = 0;
+    let even = 0;
+    for (let i = 0; i < arr.length; i++){
+        if (arr[i] % 2 === 0) even+= arr[i];
+        if (arr[i] % 2 !== 0) odd+= arr[i];
+    }
+    const object = {
+        even: even,
+        odd: odd
+    }
+    return object;
+}
+
+// 4
+function onlineOrder(product1, product2, product3, moneyAmount){
+
+    const totalPriceProduct1 = (product1['price'] * product1['quantity']);
+    const totalPriceProduct2 = (product2['price'] * product2['quantity']);
+    const totalPriceProduct3 = (product3['price'] * product3['quantity']);
+    const totalCostPrice = totalPriceProduct1 + totalPriceProduct2 + totalPriceProduct3;
+
+    if (moneyAmount >= totalCostPrice){
+        return 'You can buy all products';
+    };
+
+    if (moneyAmount >= (totalCostPrice - totalPriceProduct3)){
+        return `You have enough money to buy ${product1['name']} and ${product2['name']}`
+    }
+    if (moneyAmount >= (totalCostPrice - (totalPriceProduct3 + totalPriceProduct2))){
+        return `You have enough money to buy ${product1['name']}`
+    }
+    return "You don't have enough money";
+}
+
+// 5
+
+function orderDelivery (delivery, isDelivery, meridiem){
+    if (isDelivery) {
+        return `Your order was delivered to ${delivery['address']} on ${delivery['date']} at ${delivery['time']} ${meridiem}`
+    } else {
+        return `Your order was picked up by courier at ${delivery['time']} ${meridiem} on ${delivery['date']}`
+    }
+}
+
+// 6
+function hasProperty(obj, key){
+    return key in obj;
+}
+console.log(hasProperty({ name: "Helen", lastName: "Rock" }, "lastName"));
+
+
+
+
+
+
+
+
+
+
+
