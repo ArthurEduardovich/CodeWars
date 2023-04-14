0b500c424cc9f03f95eb328ac1eab944833d0035
ArthurEduardovich <artyr.eduardovich@gmail.com>
2023-04-14 13:44:43 +0500

    new kata

--- test2.js
+++ test2.js
@@ -1,3 +1,25 @@
-const arr = [1, 2, 1, 0, 3, 4, 5];
+// 1 recursion
+function countdown(n) {
 
-console.log(arr.lastIndexOf(1, -1))
+    if (n < 1   ) {
+        return [];
+    } else {
+        const arr = countdown(n - 1);
+        arr.unshift(n);
+        return arr;
+    }
+}
+
+// console.log(countdown(5));
+
+// 2 recursion от 6 до 9
+function rangeOfNumbers(startNum, endNum){
+    if (startNum === endNum + 1){
+        return []
+    } else {
+        const arr = rangeOfNumbers(startNum + 1, endNum);
+        arr.unshift(startNum)
+        return arr;
+    }
+}
+console.log(rangeOfNumbers(6, 9)) // 6,7,8,9
