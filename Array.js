6e7ad1ec6984668a09be5e1fdc7a0d82aa8808db
ArthurEduardovich <artyr.eduardovich@gmail.com>
2023-04-21 19:01:42 +0300

    new kata

--- Array.js
+++ Array.js
@@ -1,13 +1,4 @@
-function deepCopyArray(arr) {
-    if (!Array.isArray(arr)) {
-      return arr;
-    }
-    
-    const copy = [];
-    for (let i = 0; i < arr.length; i++) {
-      copy.push(deepCopyArray(arr[i]));
-    }
-    return copy;
-  }
-
-console.log(deepCopyArray([1, [2, [3, 4], 5], 6]))
+function digitize(n) {
+  return n.toString().split('').reverse().map((el) => Number(el))
+}
+console.log(digitize(32333))
