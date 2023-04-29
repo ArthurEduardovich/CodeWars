282714a13839864da549da3960acbc0e7c7f991c
ArthurEduardovich <artyr.eduardovich@gmail.com>
2023-04-29 22:06:27 +0300

    createPhoneNumber ver 2

--- LocalCodingQuest + codewars (kata 6 - 8)/Array/Create Phone Number (ky 6).js
+++ LocalCodingQuest + codewars (kata 6 - 8)/Array/Create Phone Number (ky 6).js
@@ -1,12 +1,22 @@
-    function createPhoneNumber(numbers){
-        const one = []
-        const two = []
-        const three = []
+function createPhoneNumber(numbers){
+        const one = [];
+        const two = [];
+        const three = [];
         for (let i = 0; i < numbers.length; i++) {
-            if (i <= 2) one.push(numbers[i])
-            if (i <= 5 && i > 2) two.push(numbers[i])
-            if (i <= 9 && i > 5) three.push(numbers[i])
-        }
-        return `(${one.join('')}) ${two.join('')}-${three.join('')}`
+            if (i <= 2) one.push(numbers[i]);
+            if (i <= 5 && i > 2) two.push(numbers[i]);
+            if (i <= 9 && i > 5) three.push(numbers[i]);
+        };
+        return `(${one.join('')}) ${two.join('')}-${three.join('')}`;
+    };
+// console.log(createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]))
+
+function createPhoneNumber2 (numbers) {
+    let format = '(xxx) xxx-xxxx';
+    for (let i = 0; i < numbers.length; i++) {
+        format = format.replace('x', numbers[i]);
     }
-console.log(createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]))
+    return format;
+};
+
+console.log(createPhoneNumber2([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]))
