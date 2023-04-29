872689ac449803f5fadc5019f390ce7731dcf8ff
ArthurEduardovich <artyr.eduardovich@gmail.com>
2023-04-29 21:56:20 +0300

    Create Phone Number (ky 6)

---
+++ LocalCodingQuest + codewars (kata 6 - 8)/Array/Create Phone Number (ky 6).js
@@ -0,0 +1,12 @@
+    function createPhoneNumber(numbers){
+        const one = []
+        const two = []
+        const three = []
+        for (let i = 0; i < numbers.length; i++) {
+            if (i <= 2) one.push(numbers[i])
+            if (i <= 5 && i > 2) two.push(numbers[i])
+            if (i <= 9 && i > 5) three.push(numbers[i])
+        }
+        return `(${one.join('')}) ${two.join('')}-${three.join('')}`
+    }
+console.log(createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]))
