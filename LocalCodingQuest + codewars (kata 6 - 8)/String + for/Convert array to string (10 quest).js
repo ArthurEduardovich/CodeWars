df45d28cb28e3fc4e46585bf198055bf4b438cac
ArthurEduardovich <artyr.eduardovich@gmail.com>
2023-05-06 13:41:44 +0500

    + 9 quest for object func

--- LocalCodingQuest + codewars (kata 6 - 8)/String + for/Convert array to string (10 quest).js
+++ LocalCodingQuest + codewars (kata 6 - 8)/String + for/Convert array to string (10 quest).js
@@ -75,4 +75,47 @@ function getWordsStartingWith(arr, word){
 }
 // console.log(getWordsStartingWith(['Ant', 'case', 'art', 'A'], 'a')); // 'Ant art A'
 
-// 8 Выбрать из массива все слова, которые имеют определенную длину
+// 8 Выбрать из массива все слова, которые содержат заданную букву
+
+function filterByLetter(words, letter) {
+    let res = '';
+    let lowerLetter = letter.toLowerCase();
+    for (let i = 0; i < words.length; i++){
+        if (letter === '') {
+            i === words.length - 1? res+= words[i]: res+= words[i] + ';';
+        } if (letter !== '') {
+            if (words[i].toLowerCase().includes(lowerLetter)){
+                if (res === '') res+= words[i]
+                else res+= ';' + words[i];
+            } else {
+                continue;
+            }
+        }
+    }
+    return res;
+}
+// console.log(filterByLetter(['West', 'work', 'core', 'rows', 'East'], 'w')); // 'West;work;rows'
+
+function booksToRead(books){
+    let booksToRead = [];
+    for (let i = 0; i < books.length; i++) {
+      if (books[i].alreadyRead === false) {
+        booksToRead.push(books[i]);
+      }
+    }
+    if (booksToRead.length === 0) {
+      return `Congrats! You've read all books.`;
+    } else {
+      let result = `You still need to read: `;
+      for (let i = 0; i < booksToRead.length; i++) {
+        result += `"${booksToRead[i].title}" by ${booksToRead[i].author}`;
+        if (i === booksToRead.length - 1) {
+          result += `.`;
+        } else {
+          result += `, `;
+        }
+      }
+      return result;
+    }
+}
+console.log(booksToRead([{title: 'A Christmas Carol', author: 'Charles Dickens', alreadyRead: false, pages: 104}, {title: 'The Night Before Christmas', author: 'Scarlett Bailey', alreadyRead: false, pages: 120}, {title: 'The Snowman', author: 'Raymond Briggs', alreadyRead: false, pages: 32}])) // 'Congrats! You've read all books.'
