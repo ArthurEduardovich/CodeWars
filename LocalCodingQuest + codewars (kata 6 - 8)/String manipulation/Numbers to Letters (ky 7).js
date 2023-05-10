bc71294e7581dd0d9100fed14fc0033d9823f0f3
ArthurEduardovich <artyr.eduardovich@gmail.com>
2023-05-10 17:30:40 +0500

    Numbers to Letters (ky 7)

--- LocalCodingQuest + codewars (kata 6 - 8)/String manipulation/Numbers to Letters (ky 7).js
+++ LocalCodingQuest + codewars (kata 6 - 8)/String manipulation/Numbers to Letters (ky 7).js
@@ -1,14 +1,15 @@
 function switcher(x){
-    // let collectionWord = [];
-    // for (let i = 96; i <= 121; i++){
-    //     collectionWord.push(String.fromCharCode(i));
-    // }
-    // return collectionWord
-    const result = [];
-    const xSort = x.map((el) => Number(el)).sort((x, y) => x - y);
-    for (let i = 0; i < xSort.length; i++){
-        result.push(String.fromCodePoint(121 - xSort[i]))
+    let collectionWord = [];
+    for (let i = 123; i >= 97; i--){
+        collectionWord.push(String.fromCharCode(i));
     }
-    return result;
+    collectionWord.push('!','?',' ')
+    const xToNumber = x.map((el) => Number(el));
+    let str = ''
+    for (let j = 0; j < xToNumber.length; j++){
+        str+= collectionWord[xToNumber[j]]
+    }
+    return str;
 }
-console.log(switcher(['24', '12', '23', '22', '4', '26', '9', '8'])); // codewars
+console.log(switcher(['24', '12', '23', '22', '4', '26', '9', '8'])); // 'codewars'
+console.log(switcher(['25','7','8','4','14','23','8','25','23','29','16','16','4'])); // 'btswmdsbd kkw'
